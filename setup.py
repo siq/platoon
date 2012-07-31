5a5f81f38a981b2102f0f562b3f7d69cfcc76aa8
Jordan McCoy <mccoy.jordan@gmail.com>
2012-07-31 10:52:20 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     name='platoon',
     version='1.0.0a1',
     packages=enumerate_packages('platoon'),
+    package_data={
+        'platoon': ['migrations/env.py', 'migrations/script.py.mako',
+            'migrations/versions/*.py'],
+    }
 )
