85c229101f56e250a8426aa891a86ed6c2c25e71
Aaron Colichia <colichia@us.ibm.com>
2015-04-15 11:36:33 -0500

    Improvement 6496 appstack service mgt. improvements

--- svc/platoon-ctl.sh
+++ svc/platoon-ctl.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-
+(
+set -x
 # Sets PYTHONPATH to siqhashlib if running FIPS enabled
-source ${BINPATH}/fips_python
+source /siq/bin/fips_python
+timeout=300
 
 case "$1" in
   start)
-    ${ENVPATH}/python/bin/bake -m spire.tasks spire.daemon config=${SVCPATH}/platoon/platoon.yaml
+    while (( $timeout > 0 )); do
+      [[ -e /var/siq/run/nucleus-services-ready ]] && break
+      #[[ -e /var/siq/run/nucleus-services-ready && $(nc -z 127.0.0.1 80) ]] && break
+      sleep 1
+      let timeout--
+    done
+    [[ $timeout == 0 ]] && exit 1
+    sleep 3
+    /siq/env/python/bin/bake -m spire.tasks spire.daemon config=/siq/svc/platoon/platoon.yaml
     ;;
   stop)
-    if [[ -e ${VARPATH}/platoon.pid ]]; then
-      /bin/bash -c '/bin/kill $(<${VARPATH}/platoon.pid)'
+    if [[ -e /var/siq/run/platoon.pid ]]; then
+      /bin/bash -c '/bin/kill $(</var/siq/run/platoon.pid)'
     fi
     ;;
 esac
+) > /var/siq/log/platoon-script.log 2>&1 &
