ea92db40f6704585e4efeedd99f56f5fc1b1077a
Aaron Colichia <colichia@us.ibm.com>
2015-04-24 16:15:55 -0500

    Improvement 6496 re-implement svcs improvements

--- svc/platoon-ctl.sh
+++ svc/platoon-ctl.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-(
-set -x
+
 # Sets PYTHONPATH to siqhashlib if running FIPS enabled
 source /siq/bin/fips_python
-timeout=300
 
 case "$1" in
   start)
-    while (( $timeout > 0 )); do
-      [[ -e /var/siq/run/nucleus-services-ready ]] && break
-      #[[ -e /var/siq/run/nucleus-services-ready && $(nc -z 127.0.0.1 80) ]] && break
-      sleep 1
-      let timeout-=1
-    done
-    [[ $timeout == 0 ]] && exit 1
-    sleep 3
     /siq/env/python/bin/bake -m spire.tasks spire.daemon config=/siq/svc/platoon/platoon.yaml
     ;;
   stop)
@@ -23,4 +13,3 @@ case "$1" in
     fi
     ;;
 esac
-) > /var/siq/log/platoon-script.log 2>&1 &
