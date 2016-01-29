7b0a83b7e07f1ee6f0abf8128f0f16e894d1c972
shola esho <sesho@us.ibm.com>
2016-01-29 16:00:45 -0500

    Improv 7822 fix for adding constructor schema changes on upgrades

--- svc/postinstall.sh
+++ svc/postinstall.sh
@@ -5,7 +5,7 @@ if [ ! -e "$logreopen" ]; then
 fi
 
 ${BINPATH}/bake -m spire.tasks spire.schema.deploy schema=platoon \
-  config=${SVCPATH}/platoon/platoon.yaml
+  config=${SVCPATH}/platoon/platoonapi.yaml
 
 # improvement 6496 need to stop before running
 if [[ -e ${VARPATH}/platoon.pid ]]; then
